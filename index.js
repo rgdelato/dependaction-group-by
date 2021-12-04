2454c4119a37b29aeb9b886c327f2a0e8b018327
Ryan De La Torre <rdelatorre@servicetitan.com>
2021-12-03 17:01:34 -0800

    adding hash functionality

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ async function groupDependenciesByScopeAndVersion(allDependencies) {
         semverLabel: semver,
         displayName:
           packages.length > 1 ? `${scope} packages` : packages[0].name,
-        slug: getSlug(packages.length > 1 ? scope : packages[0].name, latest),
+        hash: getHash(packages),
         prBody: generatePullRequestBody(packages),
       });
     }
@@ -222,7 +222,7 @@ async function groupDependenciesByScopeAndVersion(allDependencies) {
       groupLatestVersion: latest,
       semverLabel: semver,
       displayName: packageData.name,
-      slug: getSlug(packageData.name, latest),
+      hash: getHash(packages),
       prBody: generatePullRequestBody(packages),
     });
   }
@@ -377,12 +377,9 @@ function getLowerVersion(currentWithRange, latestWithRange) {
  * @param {*} version
  * @returns
  */
-function getSlug(packageName = "", version = "") {
-  return (
-    packageName.toLowerCase().replace("@", "").replace(/\W/g, "_") +
-    "-" +
-    version.replace(/\D/g, "_")
-  );
+function getHash(packages) {
+  const str = packages.map(({ name }) => name).join();
+  return unique(str);
 }
 
 /**
@@ -454,4 +451,61 @@ function wildcardToRegExp(s) {
  */
 function regExpEscape(s) {
   return s.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&");
+}
+
+// Short string hashing:
+// Source: https://github.com/bibig/node-shorthash/blob/master/shorthash.js
+
+// refer to: http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+function bitwise(str) {
+  var hash = 0;
+  if (str.length == 0) return hash;
+  for (var i = 0; i < str.length; i++) {
+    var ch = str.charCodeAt(i);
+    hash = (hash << 5) - hash + ch;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+// 10进制转化成62进制以内的进制
+// convert 10 binary to customized binary, max is 62
+function binaryTransfer(integer, binary) {
+  binary = binary || 62;
+  var stack = [];
+  var num;
+  var result = "";
+  var sign = integer < 0 ? "-" : "";
+
+  function table(num) {
+    var t = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return t[num];
+  }
+
+  integer = Math.abs(integer);
+
+  while (integer >= binary) {
+    num = integer % binary;
+    integer = Math.floor(integer / binary);
+    stack.push(table(num));
+  }
+
+  if (integer > 0) {
+    stack.push(table(integer));
+  }
+
+  for (var i = stack.length - 1; i >= 0; i--) {
+    result += stack[i];
+  }
+
+  return sign + result;
+}
+
+/**
+ * why choose 61 binary, because we need the last element char to replace the minus sign
+ * eg: -aGtzd will be ZaGtzd
+ */
+function unique(text) {
+  var id = binaryTransfer(bitwise(text), 61);
+  return id.replace("-", "Z");
 }
